87a531ffb2dc9a4f63184977e0460dbbf2219520
sparsick <sparsick@web.de>
2017-06-22 21:15:47 +0200

    docker-image-lifecycle: add clean up image step to stop registry script

--- docker-image-lifecycle/stop-docker-registry.sh
+++ docker-image-lifecycle/stop-docker-registry.sh
@@ -1,3 +1,5 @@
  #!/bin/sh
- 
+
 docker stop registry
+
+docker rmi 192.168.33.11:5000/sparsick/mysql:ansible localhost:5000/sparsick/mysql:ansible 192.168.33.11:5000/sparsick/tomcat:ansible localhost:5000/sparsick/tomcat:ansible
